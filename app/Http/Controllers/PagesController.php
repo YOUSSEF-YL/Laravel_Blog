c56a09a56771c36bc4fd4566cc371c899f42a20c
YOUSSEF-YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-05-18 23:54:04 +0100

    PagesController

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+
+    public function post()
+    {
+        return view('post');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/',[PagesController::class,'index']);
+Route::get('/',[PagesController::class,'post']);
