9c3fce4e5f7afc9c5bb82440d75172afbaefac16
YOUSSEF-YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-05-30 19:37:41 +0100

    file_sys

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,7 +53,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
         
-      
+      ##############################for image uplods #########################################
            
             'uploads' => [
                         'driver' => 'local',
