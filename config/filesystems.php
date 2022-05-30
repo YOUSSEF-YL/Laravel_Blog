869ba247a71bcd26d29f7a0d64ce29fca5665002
YOUSSEF-YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-05-30 19:29:16 +0100

    file_sys

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,7 +69,7 @@ return [
     |
     | Here you may configure the symbolic links that will be created when the
     | `storage:link` Artisan command is executed. The array keys should be
-    | the locations of the links and the values should be their targets.
+    | the locations of the links and the values should be their targets
     |
     */
 
