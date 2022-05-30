0fcf25356ba742eed7a19f79d270e0e9fad51b90
YOUSSEF-YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-05-30 23:48:55 +0100

    ArticleController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
@@ -18,8 +19,5 @@ class PagesController extends Controller
     }
 
 
-    public function post()
-    {
-        return view('post');
-    }
+    
 }
