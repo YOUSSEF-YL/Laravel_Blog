0721409e26cd65587fca078a4b305be03907f229
YOUSSEF-YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-06-03 18:35:50 +0100

    RouteServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/',[PagesController::class,'index']);
 
 
 Route::get('/Post/{Slug}',[ArticleController::class,'show']);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
