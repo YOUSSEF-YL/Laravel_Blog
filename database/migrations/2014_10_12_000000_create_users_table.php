d027661bd4d4bbf1a613f3c7c4066d1a17718b35
YOUSSEF-YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-06-07 23:53:29 +0100

    user_migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('Lastename')->nullable();
+            $table->string('username')->nullable();
+            $table->string('aboutme')->nullable();
+            $table->string('image')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
